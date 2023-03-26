9e22fc131b783f0c1d61808fae7510d145598907
Quevin Bambasi <quevinbambasi@pm.me>
2023-03-26 09:19:00 -0400

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Quevin Bambasi on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
